ba240fad973dfeb17df10b7ed91533542d8d700e
sneha <sneha.p@thegti.co.in>
2019-09-18 10:07:26 +0530

    report

--- src/app/reportModule/report/report.component.ts
+++ src/app/reportModule/report/report.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { WebDataRocksPivot } from "../WebDataRocks/webdatarocks.angular4";
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 import { ApiService } from '../../services/common/common.service';
-import {User} from '../../authentication/user.model';
+import { User } from '../../authentication/user.model';
 import { AuthService } from '../../authentication/auth.service';
 
 @Component({
@@ -18,16 +18,17 @@ export class ReportComponent {
   isdate: Boolean = false;
   isdatechange: Boolean = false;
   user: User;
- reportList: Array<Object>;
- keywordList: Array<Object>;
+  reportList: Array<Object>;
+  keywordList: Array<Object>;
+
 
 
   @ViewChild('pivot1') child: WebDataRocksPivot;
   //Ref: https://www.webdatarocks.com/doc/integration-with-angular/
-  
-    title = 'ReportDemo';
-    onPivotReady(pivot: WebDataRocks.Pivot): void {
-      console.log("[ready] WebDataRocksPivot", this.child);
+
+  title = 'ReportDemo';
+  onPivotReady(pivot: WebDataRocks.Pivot): void {
+    console.log("[ready] WebDataRocksPivot", this.child);
   }
   reportData: string;
 
@@ -37,53 +38,17 @@ export class ReportComponent {
     if (data.isGrandTotalRow) cell.addClass("fm-grand-total-r");
     if (data.isGrandTotalColumn) cell.addClass("fm-grand-total-c");
   }
-  
-  onReportComplete(): void {
-   
-  this.child.webDataRocks.off("reportcomplete");
-  this.child.webDataRocks.setReport({options:{ grid:{ showFilter:false,showHierarchyCaptions:false} },
-    dataSource: {
-      filename: "https://cdn.webdatarocks.com/data/data.json"
-    },
-        slice: { 
-          rows: [{
-                  uniqueName: "Color", 
-              },
-              {
-                  uniqueName: "Destination"
-              },
-              {
-                  uniqueName: "Business Type"
-              },
-              {
-                  uniqueName: "Country"
-              },
-              {
-                  uniqueName: "Price"
-              }
-          ], 
-          columns:[
-            {
-              uniqueName: "Country"
-            },
-            {
-                uniqueName: "Measures"
-            }
-          ],
-          measures:[{
-            uniqueName: "Price",
-            aggregation: "sum"
-        }],  },
 
-        
-  });
+  onReportComplete(): void {
+
+
   }
   constructor(
-    private _formBuilder: FormBuilder,private apiService: ApiService, public authService: AuthService,
+    private _formBuilder: FormBuilder, private apiService: ApiService, public authService: AuthService,
   ) { }
 
   ngOnInit() {
-    this.user= this.authService.getUserDetail();
+    this.user = this.authService.getUserDetail();
     this.stprMain = this._formBuilder.group({
       ddlVendorName: [null],
       ddlVendorKeyword: [null],
@@ -92,7 +57,7 @@ export class ReportComponent {
 
     });
     this.GetReports();
-   
+
   }
   enbleSearch(): any {
     this.enableSearch = true;
@@ -100,43 +65,72 @@ export class ReportComponent {
   }
   onChange(event): void {
     if (event.value == null) {
-        this.isdate = true;
+      this.isdate = true;
     }
-}
-onDateChange(event): void {
-  if (event.value == null) {
+  }
+  onDateChange(event): void {
+    if (event.value == null) {
       this.isdatechange = true;
+    }
   }
-}
-filterReports(): void {
-  
-  this.enableSearch = false;
-  var reqObj = {
-   
-    "VND_PK":this.user.VND_PK,
-    'FROM_DATE': this.stprMain.value.txtFromDob,
-    'TO_DATE': this.stprMain.value.txtToDob,
-  };
- 
-  this.apiService.GetVendorKeywords(reqObj).subscribe((data: Array<object>) => {
-    this.keywordList = data['Data'];
-    console.log(this.reportList);
+  filterReports(): void {
+    this.enableSearch = false;
+    var reqObj = {
+      "VND_PK": this.user.VND_PK,
+      'FROM_DATE': this.stprMain.value.txtFromDob,
+      'TO_DATE': this.stprMain.value.txtToDob,
+    };
+    //console.log(reqObj);
+    this.apiService.GetVendorKeywords(reqObj).subscribe((data: Array<object>) => {
+      this.keywordList = data['Data'];
+      this.BindReportVendorKeywords(this.keywordList);
+    });
+  }
 
-});
- 
-}
-GetReports() {
-  const reqbody = {
-      'RPT_TYPE': 1,
-     
-  };
-  this.apiService.GetReports(reqbody).subscribe((data: Array<object>) => {
+  BindReportVendorKeywords(rptdata) {
+    this.child.webDataRocks.off("reportcomplete");
+    this.child.webDataRocks.setReport({
+      options: { grid: { showFilter: false, showHierarchyCaptions: true } },
+      dataSource: {
+        data: rptdata
+      },
+      slice: {
+        rows: [
+          {
+            uniqueName: "KEYWORD",
+            caption: "Keywords."
+          }
+        ],
+        columns: [
+          {
+            uniqueName: "Measures"
+          }
+        ],
+        measures: [{
+          uniqueName: "LEADS_COUNT",
+          //aggregation: "sum",
+          grandTotalCaption: "Leads Count"
+        }],
+      }
+    });
+
+  }
+  GetReports() {
+
+    if (this.user.ROL_PK == 1) {
+      var reqObj = { 'RPT_TYPE': 1 };
+
+
+    }
+    else {
+      var reqObj = { 'RPT_TYPE': 2 };
+    }
+
+    this.apiService.GetReports(reqObj).subscribe((data: Array<object>) => {
       this.reportList = data['Data'];
-      console.log(this.reportList);
-  
-  });
-}
-
+    });
 
   }
-  
+
+}
+
